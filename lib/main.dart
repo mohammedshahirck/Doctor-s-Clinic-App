544183a5935f67b9fe7e8e4eaa0d9c4094852007
mohammedshahirck <mohammedshahirck@gmail.com>
2023-03-12 10:22:19 +0530

    firebase_setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:doctor_clinic/provider/register_provider.dart';
 import 'package:doctor_clinic/screens/onbord/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
